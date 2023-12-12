bb299889f0f3c8aff4792231078c09d6877c737a
Alona Chubenko <aliona.chubenko@gmail.com>
2023-12-12 10:59:43 +0200

    Fix CSS style for Navlinks

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,16 +9,28 @@ export default function Header({ cleanSearch }) {
 
     return (
         <header>
-            <h1><NavLink className="header" to="/">Zoo REACT App</NavLink></h1>
+
             <nav>
-                <ul>
-                    <li><NavLink to="/">Home</NavLink></li>
-                    <li><NavLink to="/animals" onClick={cleanSearch}>Animals</NavLink></li>
-                    <li><NavLink to="/birds" onClick={cleanSearch}>Birds</NavLink></li>
-                    <li><NavLink to="/about">About</NavLink></li>
-                </ul>
+                <div className='navLinkGroup'>
+                    <p><NavLink className="header" to="/">Zoo REACT App</NavLink></p>
+                    <ul className='liContainer'>
+                        <li><NavLink to="/">Home</NavLink></li>
+                        <li><NavLink to="/animals" onClick={cleanSearch}>Animals</NavLink></li>
+                        <li><NavLink to="/birds" onClick={cleanSearch}>Birds</NavLink></li>
+                        <li><NavLink to="/about">About</NavLink></li>
+                    </ul>
+                </div>
             </nav>
 
         </header>
     );
 };
+
+/*
+export default function Header({ cleanSearch }) {
+
+    /*const { pathname } = useLocation();
+    if (pathname === "/")
+        return null;*/
+
+
