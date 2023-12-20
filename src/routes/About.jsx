f3658a3bcc17ffb9f21d2f584f9d3495d8b91d2b
Alona Chubenko <aliona.chubenko@gmail.com>
2023-12-20 14:14:22 +0200

    Change css style

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -2,7 +2,7 @@ export default function About() {
     return (
         <div className="about">
             <h1>This is  a Zoo project made in React</h1>
-            <h2>This project was done in Business College</h2>
+            <h2>This project was done in Business College of Helsinki</h2>
             <h3>Such tools were used:</h3>
             <ul>
                 <li>
--- src/routes/CategoryPage.jsx
+++ src/routes/CategoryPage.jsx
@@ -2,6 +2,7 @@ import { useParams } from 'react-router-dom';
 import Card from '../components/Card';
 import Search from '../components/Search.jsx';
 import BackToTop from "../components/BackToTop";
+import Footer from '../components/Footer';
 
 export default function CategoryPage(props) {
     const params = useParams();
@@ -12,25 +13,27 @@ export default function CategoryPage(props) {
 
     return (
         <>
-            <h1>{category.toUpperCase()}</h1>
-            <Search {...props} />
+            <div>
+                <h1>{category.toUpperCase()}</h1>
+                <Search {...props} />
 
-            <div className='cards'>
-                {props[category]
-                    .filter((element) =>
-                        element.name
-                            .toLowerCase()
-                            .includes(props.search.toLowerCase()))
-                    .map((element) => (
-                        <Card key={element.name}
-                            {...element} // pass all the object
-                            onclick={() =>
-                                props.closeHandler(element.name, category)}
-                            addLikes={() => props.likesCounter(element.name, 'add', category)} //addLikes is a trigger
-                            removeLikes={() => props.likesCounter(element.name, 'remove', category)}
-                        />))}
+                <div className='cards'>
+                    {props[category]
+                        .filter((element) =>
+                            element.name
+                                .toLowerCase()
+                                .includes(props.search.toLowerCase()))
+                        .map((element) => (
+                            <Card key={element.name}
+                                {...element} // pass all the object
+                                onclick={() =>
+                                    props.closeHandler(element.name, category)}
+                                addLikes={() => props.likesCounter(element.name, 'add', category)} //addLikes is a trigger
+                                removeLikes={() => props.likesCounter(element.name, 'remove', category)}
+                            />))}
+                </div>
+                <BackToTop />
             </div>
-            <BackToTop />
         </>
     )
 };
